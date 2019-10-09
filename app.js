d3427a8e360910cc96c1588d80598b4b2cb458d6
ivan1120 <chenivan5@gmail.com>
2019-10-08 23:28:02 -0700

    change master branch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const bodyparser = require('body-parser');
 const login = require('./routes/login.js');
 const register = require('./routes/register.js');
-const home = require('./routes/home.js');
+
 const path = require('path');
 const port = process.env.PORT || 3000
 const DB=require('./modules/db.js');  
@@ -25,15 +25,26 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.use((req, res) => {
-    var data={"fName":"Kag"};
-    DB.find('User',data,(err,data)=>{
-        console.log(data);
-    })
-
+app.get('/',(req, res) => {
+    // var data={"fName":"Kag"};
+    // DB.find('User',data,(err,data)=>{
+    //     console.log(data);
+    // })
     res.render('index');
-   
+})
 
+app.get('/findTutor',(req, res) => {
+    res.render('findTutor');
+})
+
+app.get('/becomeTutor',(req, res) => {
+    res.render('becomeTutor');
+})
+app.get('/signUp',(req, res) => {
+    res.render('signUp');
+})
+app.get('/useProfile',(req, res) => {
+    res.render('userProfile');
 })
 
 
