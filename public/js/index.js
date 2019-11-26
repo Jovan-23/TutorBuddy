fe96135a59d9ae8fd340c9900312d9241608ad76
Jovan Sekhon <jovansekhon@hotmail.com>
2019-11-26 10:04:15 -0800

    Homepage fix

--- public/js/index.js
+++ public/js/index.js
@@ -22,5 +22,27 @@ $(function() {
     $('.open-menu').height($(window).height());
   });
   
-  
-  
+// changes the become tutor image
+function change1() {
+  document.getElementById("changing_image").src="/img/homepage20.jpg";
+}
+
+// changes the find tutor image
+function change2() {
+  document.getElementById("changing_image").src="/img/homepage19.jpg";
+}
+
+// changes the view tutor image
+function change3() {
+  document.getElementById("changing_image").src="/img/homepage18.jpg";
+}
+
+// changes the post schedule image
+function change4() {
+  document.getElementById("changing_image").src="/img/homepage.jpg";
+}
+
+// changes the profile image
+function change5() {
+  document.getElementById("changing_image").src="/img/showcase3.jpg";
+}
