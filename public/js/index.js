51eff54dacbb3bb0ef19bc5f2b99929ff5b35b44
TanushParkashVerma <tanush.parkash16@gmail.com>
2019-12-01 17:10:20 -0800

    new icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,61 +1,61 @@
 // Responsive Nav
-$(function () {
-  menu = $('nav ul');
-  $('#openup').on('click', function (e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
+$(function() {
+    menu = $('nav ul');
+    $('#openup').on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
 
-  $(window).resize(function () {
-    var w = $(this).width();
-    if (w > 480 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
+    $(window).resize(function() {
+        var w = $(this).width();
+        if (w > 480 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
 
-  $('nav li').on('click', function (e) {
-    var w = $(window).width();
-    if (w < 480) {
-      menu.slideToggle();
-    }
-  });
-  $('.open-menu').height($(window).height());
+    $('nav li').on('click', function(e) {
+        var w = $(window).width();
+        if (w < 480) {
+            menu.slideToggle();
+        }
+    });
+    $('.open-menu').height($(window).height());
 });
 
 /** Same as above but for the resposive nav bar when below 680px */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsId("openup").style.top = "0px";
-  } else {
-    document.getElementsById("openup").style.top = "-300px";
-  }
-  prevScrollpos = currentScrollPos;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementsId("openup").style.top = "0px";
+    } else {
+        document.getElementsById("openup").style.top = "-300px";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
 // changes the become tutor image
 function change1() {
-  document.getElementById("changing_image").src = "/img/homepage20.jpg";
+    document.getElementById("changing_image").src = "/img/screenshot1.jpg";
 }
 
 // changes the find tutor image
 function change2() {
-  document.getElementById("changing_image").src = "/img/homepage19.jpg";
+    document.getElementById("changing_image").src = "/img/homepage19.jpg";
 }
 
 // changes the view tutor image
 function change3() {
-  document.getElementById("changing_image").src = "/img/homepage18.jpg";
+    document.getElementById("changing_image").src = "/img/homepage18.jpg";
 }
 
 // changes the post schedule image
 function change4() {
-  document.getElementById("changing_image").src = "/img/homepage.jpg";
+    document.getElementById("changing_image").src = "/img/homepage.jpg";
 }
 
 // changes the profile image
 function change5() {
-  document.getElementById("changing_image").src = "/img/showcase3.jpg";
+    document.getElementById("changing_image").src = "/img/showcase3.jpg";
 }
